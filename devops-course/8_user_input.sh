8adf998d0db0d44064b07235819d271abfc77c1b
Yahaya Azeez <azconcept2016@gmail.com>
2025-04-15 11:29:30 +0100

    add user input

---
+++ devops-course/8_user_input.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 15/4/2025
+
+echo "Enter your skills:"
+read SKILL
+
+echo "Your $SKILL is in high Demand in the IT Industry."
+
+read -p "Username: " USR
+read -sp "Password: " pass
+
+echo
+
+echo "Login Succefull Welcome USER $USR"
