b16bc72810110f365db4f6569a0b29c092260d33
Yahaya Azeez <azconcept2016@gmail.com>
2025-04-15 10:57:24 +0100

    add exporting variable

---
+++ devops-course/7_exporting_variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 15/4/2025
+# exporting variable: will make the variable available
+# to all the child shell from parent shell
+# each user has ~/.bashrc or ~/.profile file where you can make variable permanent
+# and to make it globally available for all users you will edit /etc/profile file
+echo "The $SEASON season is more than expected, this time."
