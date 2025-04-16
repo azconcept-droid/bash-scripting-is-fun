107d7e52f2a2115d12a566f8aad633282279f928
Yahaya Azeez <azconcept2016@gmail.com>
2025-04-16 14:20:51 +0100

    add bash script operators

---
+++ devops-course/10_operator.sh
@@ -0,0 +1,17 @@
+# Operator
+# !
+# -n STRING: the lenght of string > 0
+# -z string: the len of str in empty or 0
+# STRING1 = STRING2: STRING1 is = STRING2
+# STRING1 != STRING2: STRING1 is not = STRING2
+# INTEGER1 -eq INTEGER2: INTEGER1 is numerically = INTEGER2
+# INTEGER1 -gt INTEGER2: INTEGER1 is numerically > INTEGER2
+# INTEGER1 -lt INTEGER2: INTEGER1 is numerically < INTEGER2
+# -d FILE: FILE exits and is a directory
+# -e FILE: FILE exits
+# -r FILE: FILE exists and the read permision is granted
+# s FILE: FILE exists and its size is > 0 (not empty)
+# -w FILE: FILE exists and the write permission is granted
+# -x FILE: FILE exits and the execute permission is granted
+
+# whenever process is running there is pid file created at /var/run/
