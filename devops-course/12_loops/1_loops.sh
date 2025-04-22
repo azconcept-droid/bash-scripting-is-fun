656ace54be3b3f71053f306dfd67d77bb996b153
Yahaya Azeez <azconcept2016@gmail.com>
2025-04-22 10:24:00 +0100

    add bash for loop

---
+++ devops-course/12_loops/1_loops.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Date: 22/4/2025
+# run for loop
+
+for VAR1 in java .net python ruby php
+do
+  echo "Looping..."
+  sleep 1
+  echo "########################################"
+  echo "Value of VAR1 is $VAR1."
+  echo "########################################"
+  date
+done
